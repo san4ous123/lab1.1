25e7222a322672486c629400fdbecd02fca5ead8
san4ous123 <78911591+san4ous123@users.noreply.github.com>
2022-03-10 16:36:44 +0700

    lab

---
+++ Lab1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class HW1
+{
+    public static long QueueTime(int[] customers, int n)
+    {
+        int theTime = 0;
+        while (customers.Length > 0)
+        {
+            int cassazan = 0;
+            if (n > customers.Length)
+            {
+                cassazan = customers.Length;
+            }
+            else
+            {
+                cassazan = n;
+            }
+
+            for (int x = 0; x < cassazan; x++)
+            {
+                customers[x]--;
+            }
+            customers = Array.FindAll(customers, y => y != 0);
+
+            theTime++;
+        }
+        return theTime;
+    }
+}
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine(HW1.QueueTime(new int[] { 5, 3, 4 }, 1) == 12);
+        Console.WriteLine(HW1.QueueTime(new int[] { 10, 2, 3, 3 }, 2) == 10);
+        Console.WriteLine(HW1.QueueTime(new int[] { 2, 3, 10 }, 2) == 12);
+    }
+}
